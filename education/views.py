d486725e0b2c32aa0a7d2b9797842914a12295d0
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2024-06-19 16:01:19 +0200

    feat: education instructor basic CRUD

--- education/views.py
+++ education/views.py
@@ -4,10 +4,15 @@ from rest_framework.response import Response
 from user.models import User
 from user.permissions import IsInstructor, IsCfa
 from education.models import Training
-from education.serializers import TrainingSerializer, CfaStudentSerializer
+from education.serializers import TrainingSerializer, CfaStudentSerializer, \
+    CfaInstructorSerializer
 
 
 class TrainingViewSet(viewsets.ViewSet):
+    """
+    ViewSet for CFA (or Instructor) to manage training
+    """
+
     serializer_class = TrainingSerializer
     model = Training
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
@@ -74,6 +79,10 @@ class TrainingViewSet(viewsets.ViewSet):
 
 
 class CfaStudentViewSet(viewsets.ViewSet):
+    """
+    ViewSet for CFA to manage students
+    """
+
     serializer_class = CfaStudentSerializer
     model = User
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
@@ -134,6 +143,76 @@ class CfaStudentViewSet(viewsets.ViewSet):
                 status=status.HTTP_404_NOT_FOUND)
         self.perform_destroy(instance)
         return Response(data={"message": "Student deleted"}, status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+
+
+class CfaInstructorViewSet(viewsets.ViewSet):
+    """
+    ViewSet for CFA to manage instructors
+    """
+
+    serializer_class = CfaInstructorSerializer
+    model = User
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+    permission_classes = [IsCfa]
+
+    def list(self, request):
+        queryset = self.model.objects.filter(cfa=request.user, role=2).all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        instructor = self.model.objects.filter(cfa=request.user, role=2, pk=pk).first()
+        if not instructor:
+            return Response({"message":"Instructor not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = self.serializer_class(instructor)
+        return Response(serializer.data)
+
+    def create(self, request):
+        context = {'request': request}
+        serializer = self.serializer_class(data=request.data, context=context)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        context = {'request': request}
+        instance = self.model.objects.filter(pk=pk, cfa=request.user, role=2).first()
+        if not instance:
+            return Response(
+                data={"message": "Instructor not found"},
+                status=status.HTTP_404_NOT_FOUND)
+        serializer = self.serializer_class(instance, data=request.data, context=context)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        context = {'request': request}
+        instance = self.model.objects.filter(pk=pk, cfa=request.user, role=2).first()
+        if not instance:
+            return Response(
+                data={"message": "Instructor not found"},
+                status=status.HTTP_404_NOT_FOUND)
+        serializer = self.serializer_class(instance, data=request.data, partial=True, context=context)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        instance = self.model.objects.filter(pk=pk, cfa=request.user, role=2).first()
+        if not instance:
+            return Response(
+                data={"message": "Instructor not found"},
+                status=status.HTTP_404_NOT_FOUND)
+        self.perform_destroy(instance)
+        return Response(data={"message": "Instructor deleted"}, status=status.HTTP_204_NO_CONTENT)
 
     def perform_destroy(self, instance):
         instance.delete()
