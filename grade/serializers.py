6cd806a1e5d932a63d56c39d5549a9e2870dcb2e
JulienBois <71647803+JulienBois@users.noreply.github.com>
2024-06-19 11:15:46 +0200

    Added Grade serializer

---
+++ grade/serializers.py
@@ -0,0 +1,32 @@
+import datetime
+from django.utils import timezone
+from rest_framework import serializers
+from grade.models import Grade
+
+
+class GradeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Grade
+        fields = '__all__'
+
+    def is_valid(self, *, raise_exception=False):
+        user = self.context['request'].user
+        self.initial_data['user'] = user.id
+
+        if 'instructor' in self.initial_data:
+            if user.role < 2:
+                self.initial_data['instructor'] = None
+
+        return super().is_valid(raise_exception=raise_exception)
+
+    def validate(self, attrs):
+        instructor = attrs.get('instructor')
+        if instructor and instructor.role < 2:
+            raise serializers.ValidationError({
+                'instructor': 'Instructor must have a role of 2 or higher'
+            })
+        if attrs.get('value') < 0 or attrs.get('value') > 20:
+            raise serializers.ValidationError({
+                'value': 'Value must be between 0 and 20'
+            })
+        return attrs
