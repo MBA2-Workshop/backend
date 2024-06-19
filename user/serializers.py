fae3641b98ad3b349a65f3532501ce2fad9625aa
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2024-06-19 22:37:43 +0200

    fix: signup role CFA

--- user/serializers.py
+++ user/serializers.py
@@ -23,7 +23,8 @@ class UserSignupSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             email=validated_data['email'],
             first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
+            last_name=validated_data['last_name'],
+            role=3
         )
         user.set_password(validated_data['password'])
         user.save()
