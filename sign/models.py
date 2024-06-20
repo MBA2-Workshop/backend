a224217655cebee2586cfc26f9036853102e9116
JulienBois <71647803+JulienBois@users.noreply.github.com>
2024-06-20 11:26:35 +0200

    Created the sign Model

---
+++ sign/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Sign(models.Model):
+    label = models.CharField(max_length=255)
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    training = models.ForeignKey('education.Training', on_delete=models.CASCADE, null=True, blank=True)
+    instructor = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='instructor', null=True, blank=True)
+    date = models.DateTimeField(null= True, blank=True)
+    signed = models.BooleanField(default=False)
